860fa3f28163e9b70ed9ca629dc32580aecd21b5
Ben Scholar <scholar@case.edu>
2022-10-26 18:15:10 -0400

    progress on lots

--- src/ariac_node.cpp
+++ src/ariac_node.cpp
@@ -34,6 +34,13 @@ using ImageCallback = boost::function<void(LogicalCameraPtr)>;
 // constants
 constexpr int k_buffer_size = 16;
 
+// global services
+ros::ServiceClient g_material_location_client;
+
+// global transform listener
+tf2_ros::Buffer g_tf_buf;
+// tf2_ros::TransformListener g_tf_listener(g_tf_buf);
+
 // global data
 std::queue<osrf_gear::Order> g_orders;
 
@@ -41,7 +48,50 @@ LogicalCameraArray<6> g_bin_images;
 LogicalCameraArray<2> g_agv_images;
 LogicalCameraArray<2> g_quality_images;
 
-void order_callback(const osrf_gear::Order &order) { g_orders.push(order); }
+void adjust_pose(geometry_msgs::PoseStamped &pose) {
+  pose.pose.position.z += 0.1;
+
+  pose.pose.orientation.w = 0.707;
+  pose.pose.orientation.x = 0.0;
+  pose.pose.orientation.y = 0.707;
+  pose.pose.orientation.z = 0.0;
+}
+
+geometry_msgs::TransformStamped
+get_robot_to_frame(const std::string &to_frame) {
+  geometry_msgs::TransformStamped tf;
+  try {
+    tf = g_tf_buf.lookupTransform("arm1_base_frame", to_frame, ros::Time(0),
+                                  ros::Duration(1.0));
+  } catch (tf2::TransformException &ex) {
+    ROS_ERROR("%s", ex.what());
+  }
+  return tf;
+}
+
+void process_order(osrf_gear::Order &order) {
+  osrf_gear::GetMaterialLocations get_loc;
+  for (const auto &s : order.shipments) {
+    for (const auto &p : s.products) {
+      get_loc.request.material_type = p.type;
+      const bool send = g_material_location_client.call(get_loc);
+      if (!send) {
+        ROS_ERROR("Could not call material location client");
+        continue;
+      }
+
+      for (const osrf_gear::StorageUnit &su : get_loc.response.storage_units) {
+        ROS_INFO("Product %s is in %s", p.type, su.unit_id);
+      }
+    }
+  }
+}
+
+void order_callback(const osrf_gear::Order &order) {
+  static int n = 0;
+  ROS_INFO("Recieved order %d", n++);
+  g_orders.push(order);
+}
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "ariac_node");
@@ -73,16 +123,19 @@ int main(int argc, char **argv) {
   // subscribe to order topic
 
   // material location service client
-  auto material_location_client =
+  g_material_location_client =
       nh.serviceClient<osrf_gear::GetMaterialLocations>(
           "/ariac/material_locations");
+  g_material_location_client.waitForExistence(ros::Duration(-1.0));
 
   auto order_sub = nh.subscribe("/ariac/orders", 2, order_callback);
 
+  // arrays of subsribers
   std::array<ros::Subscriber, 6> bin_camera_subs;
   std::array<ros::Subscriber, 2> agv_camera_subs;
   std::array<ros::Subscriber, 2> quality_camera_subs;
 
+  ROS_INFO("Subsribing to bin cameras");
   // generate subsribers to bin camera
   for (int i = 1; i <= 6; ++i) {
     const std::string topic = "/ariac/logical_camera_bin" + std::to_string(i);
@@ -95,6 +148,7 @@ int main(int argc, char **argv) {
         nh.subscribe<osrf_gear::LogicalCameraImage>(topic, 16, callback);
   }
 
+  ROS_INFO("Subsribing to agv and  quality cameras");
   // generate subscribers for agv and quality camera
   for (int i = 1; i <= 2; ++i) {
     const std::string agv_topic =
@@ -118,8 +172,8 @@ int main(int argc, char **argv) {
   }
 
   // initialize transform stuff
-  tf2_ros::Buffer tfBuffer;
-  tf2_ros::TransformListener tfListener(tfBuffer);
+  // tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(g_tf_buf);
 
   ros::Rate r(10);
 
