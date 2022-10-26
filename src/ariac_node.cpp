4088463f4639a9e0a6101fcde3cc24e5569aeb2b
Ben Scholar <scholar@case.edu>
2022-10-26 16:18:48 -0400

    lots of progress on node

--- src/ariac_node.cpp
+++ src/ariac_node.cpp
@@ -7,6 +7,12 @@
 #include "osrf_gear/Product.h"
 #include "osrf_gear/Shipment.h"
 
+#include "geometry_msgs/TransformStamped.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_ros/transform_listener.h"
+
+#include "geometry_msgs/Pose.h"
+
 #include <std_srvs/Trigger.h>
 
 #include <array>
@@ -15,11 +21,31 @@
 #include <queue>
 #include <string>
 
+using osrf_gear::LogicalCameraImage;
+
+template <size_t N>
+using LogicalCameraArray = std::array<LogicalCameraImage, N>;
+
+using LogicalCameraPtr = const boost::shared_ptr<
+    const osrf_gear::LogicalCameraImage_<std::allocator<void>>>;
+
+using ImageCallback = boost::function<void(LogicalCameraPtr)>;
+
+// constants
+constexpr int k_buffer_size = 16;
+
+// global data
 std::queue<osrf_gear::Order> g_orders;
+
+LogicalCameraArray<6> g_bin_images;
+LogicalCameraArray<2> g_agv_images;
+LogicalCameraArray<2> g_quality_images;
 
 void order_callback(const osrf_gear::Order &order) { g_orders.push(order); }
 
-bool start_competition() {
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "ariac_node");
+
   ros::NodeHandle nh;
 
   ros::ServiceClient begin_client =
@@ -27,41 +53,75 @@ bool start_competition() {
 
   std_srvs::Trigger begin_comp;
 
-  bool send_success = begin_client.call(begin_comp);
+  const bool send_success = begin_client.call(begin_comp);
 
-  if (send_success) {
-    ROS_INFO("Competition service called successfully: %s",
-             begin_comp.response.message.c_str());
-  } else {
-    ROS_ERROR("Could not start competition");
-  }
-
-  return send_success;
-}
-
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "ariac_node");
-
-  ros::NodeHandle nh;
-
-  if (!start_competition()) {
+  if (!send_success) {
+    ROS_ERROR("Competition service call failed!  Goodness Gracious!!");
     return 1;
   }
 
+  const bool start_success = begin_comp.response.success;
+
+  if (!start_success) {
+    ROS_WARN("Competition service returned failure: %s",
+             begin_comp.response.message.c_str());
+    // TODO: Should this exit?
+  } else {
+    ROS_INFO("Competition service called successfully: %s",
+             begin_comp.response.message.c_str());
+  }
   // subscribe to order topic
-  auto order_sub = nh.subscribe("/ariac/orders", 2, order_callback);
 
   // material location service client
   auto material_location_client =
       nh.serviceClient<osrf_gear::GetMaterialLocations>(
           "/ariac/material_locations");
 
-  const std::array<const char *, 10> logical_camera_names = {
-      "logical_camera_bin1",      "logical_camera_bin2",
-      "logical_camera_bin3",      "logical_camera_bin4",
-      "logic_camera_bin5",        "logical_camera_bin6",
-      "logical_camera_agv1",      "logical_camera_agv2",
-      "quality_control_sensor_1", "quality_control_sensor_2"};
+  auto order_sub = nh.subscribe("/ariac/orders", 2, order_callback);
+
+  std::array<ros::Subscriber, 6> bin_camera_subs;
+  std::array<ros::Subscriber, 2> agv_camera_subs;
+  std::array<ros::Subscriber, 2> quality_camera_subs;
+
+  // generate subsribers to bin camera
+  for (int i = 1; i <= 6; ++i) {
+    const std::string topic = "/ariac/logical_camera_bin" + std::to_string(i);
+
+    const ImageCallback callback = [&, i](LogicalCameraPtr img) {
+      g_bin_images[i - 1] = *img;
+    };
+
+    bin_camera_subs[i - 1] =
+        nh.subscribe<osrf_gear::LogicalCameraImage>(topic, 16, callback);
+  }
+
+  // generate subscribers for agv and quality camera
+  for (int i = 1; i <= 2; ++i) {
+    const std::string agv_topic =
+        "/ariac/logical_camera_agv" + std::to_string(i);
+    const std::string quality_topic =
+        "/ariac/quality_control_sensor_" + std::to_string(i);
+
+    const ImageCallback agv_callback = [&, i](LogicalCameraPtr img) {
+      g_agv_images[i - 1] = *img;
+    };
+
+    const ImageCallback quality_callback = [&, i](LogicalCameraPtr img) {
+      g_quality_images[i - 1] = *img;
+    };
+
+    agv_camera_subs[i - 1] = nh.subscribe<osrf_gear::LogicalCameraImage>(
+        agv_topic, 16, agv_callback);
+
+    quality_camera_subs[i - 1] = nh.subscribe<osrf_gear::LogicalCameraImage>(
+        quality_topic, 16, quality_callback);
+  }
+
+  // initialize transform stuff
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+
+  ros::Rate r(10);
 
   ros::spin();
 
