841ed5424ad3c6cdb093b5ee5b47ae0a9d5e0400
Ben Scholar <scholar@case.edu>
2022-11-08 17:44:13 -0500

    working on lab 6

--- src/ariac_node.cpp
+++ src/ariac_node.cpp
@@ -7,6 +7,12 @@
 #include "osrf_gear/Product.h"
 #include "osrf_gear/Shipment.h"
 
+#include "sensor_msgs/JointState.h"
+
+#include "trajectory_msgs/JointTrajectory.h"
+
+#include "ur_kinematics/ur_kin.h"
+
 #include "geometry_msgs/TransformStamped.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "tf2_ros/transform_listener.h"
@@ -39,14 +45,27 @@ ros::ServiceClient g_material_location_client;
 
 // global transform listener
 tf2_ros::Buffer g_tf_buf;
-// tf2_ros::TransformListener g_tf_listener(g_tf_buf);
+// tf2_ros::TransformListener g_tf_listener(g_tf_buf);You will find it
+// challenging to perform as well as those attending which could have a
+// detrimental effect on your grade.
 
 // global data
 std::queue<osrf_gear::Order> g_orders;
+sensor_msgs::JointState g_joint_state;
 
 LogicalCameraArray<6> g_bin_images;
 LogicalCameraArray<2> g_agv_images;
 LogicalCameraArray<2> g_quality_images;
+
+bool close_to(double *a, double *b, int n) {
+  bool eq = true;
+  const double threshold = 0.01;
+  for (int i = 0; i < n; i++) {
+    const double diff = std::abs(a[i] - b[i]);
+    eq &= (diff < threshold);
+  }
+  return eq;
+}
 
 void adjust_pose(geometry_msgs::PoseStamped &pose) {
   pose.pose.position.z += 0.1;
@@ -122,6 +141,11 @@ void order_callback(const osrf_gear::Order &order) {
   g_orders.push(order);
 }
 
+void joint_state_callback(const sensor_msgs::JointState &msg) {
+  ROS_INFO_THROTTLE(10, "Recieved joint_state"); // TODO: do this
+  g_joint_state = msg;
+}
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "ariac_node");
 
@@ -158,6 +182,10 @@ int main(int argc, char **argv) {
   g_material_location_client.waitForExistence(ros::Duration(-1.0));
 
   auto order_sub = nh.subscribe("/ariac/orders", 2, order_callback);
+  auto joint_state_sub =
+      nh.subscribe("/ariac/arm1/joint_states", 128, joint_state_callback);
+  auto traj_pub =
+      nh.advertise<trajectory_msgs::JointTrajectory>("/ariac/arm1/command", 2);
 
   // arrays of subsribers
   std::array<ros::Subscriber, 6> bin_camera_subs;
@@ -204,14 +232,72 @@ int main(int argc, char **argv) {
   // tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener tfListener(g_tf_buf);
 
-  ros::Rate r(10);
+  double q_pose[6], q_des[6][8];
+  double T_current[4][4], T_des[4][4];
 
+  int traj_count = 0;
+
+  ros::Rate r(10);
   while (ros::ok()) {
     if (g_orders.size() > 0) {
       const auto order = g_orders.front();
       process_order(order);
       g_orders.pop();
     }
+
+    for (int i = 0; i < 6; ++i) {
+      q_pose[i] = g_joint_state.position[i + 1];
+    }
+
+    ur_kinematics::forward(q_pose, &T_current[0][0]);
+
+    if (close_to(&T_current[0][0], &T_des[0][0], 16)) {
+      T_des[0][3] = desired.pose.position.x;
+      T_des[1][3] = desired.pose.position.y;
+      T_des[2][3] = desired.pose.position.z + 0.3; // above part
+      T_des[3][3] = 1.0;
+      // The orientation of the end effector so that the end effector is down.
+      T_des[0][0] = 0.0;
+      T_des[0][1] = -1.0;
+      T_des[0][2] = 0.0;
+      T_des[1][0] = 0.0;
+      T_des[1][1] = 0.0;
+      T_des[1][2] = 1.0;
+      T_des[2][0] = -1.0;
+      T_des[2][1] = 0.0;
+      T_des[2][2] = 0.0;
+      T_des[3][0] = 0.0;
+      T_des[3][1] = 0.0;
+      T_des[3][2] = 0.0;
+
+      int num_sols = ur_kinematics::inverse((double *)&T_des, (double *)&q_des);
+      trajectory_msgs::JointTrajectory joint_trajectory;
+
+      joint_trajectory.header.seq = traj_count++;
+      joint_trajectory.header.stamp =
+          ros::Time::now(); // When was this message created.
+      joint_trajectory.header.frame_id =
+          "/world"; // Frame in which this is specified.
+      // Set the names of the joints being used.  All must be present.
+      joint_trajectory.joint_names.clear();
+      joint_trajectory.joint_names.push_back("linear_arm_actuator_joint");
+      joint_trajectory.joint_names.push_back("shoulder_pan_joint");
+      joint_trajectory.joint_names.push_back("shoulder_lift_joint");
+      joint_trajectory.joint_names.push_back("elbow_joint");
+      joint_trajectory.joint_names.push_back("wrist_1_joint");
+      joint_trajectory.joint_names.push_back("wrist_2_joint");
+      joint_trajectory.joint_names.push_back("wrist_3_joint");
+
+      joint_trajectory.points.resize(2);
+
+      joint_trajectory.points[1].positions.resize(
+          joint_trajectory.joint_names.size());
+      joint_trajectory.points[0].time_from_start = ros::Duration(0.0);
+      joint_trajectory.points[1].time_from_start = ros::Duration(1.0);
+
+      traj_pub.publish(&joint_trajectory);
+    }
+
     ros::spinOnce();
     r.sleep();
   }
